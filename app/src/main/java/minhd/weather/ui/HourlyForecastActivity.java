59c900d5aae063cf15e9745b80f08ccec9b499ca
Minh Duc Nguyen <dekarvn@gmail.com>
2015-12-05 09:41:34 +1100

    LayoutManager for recycler view

--- app/src/main/java/minhd/weather/ui/HourlyForecastActivity.java
+++ app/src/main/java/minhd/weather/ui/HourlyForecastActivity.java
@@ -3,29 +3,43 @@ package minhd.weather.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.Arrays;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import minhd.weather.R;
+import minhd.weather.adapters.HourAdapter;
 import minhd.weather.model.Hour;
 
 public class HourlyForecastActivity extends AppCompatActivity {
 
     private Hour[] hours;
 
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
 
+        ButterKnife.bind(this);
+
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
-        hours = Arrays.copyOf(parcelables, parcelables.length, null);
+        hours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(hours);
+        recyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setHasFixedSize(true);
     }
 
 }
